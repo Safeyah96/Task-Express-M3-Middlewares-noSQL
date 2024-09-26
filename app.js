2e2567af3b5665633eb9ea09d58cab8ece50ad82
salqabandi <saf.qab96@gmail.com>
2024-09-26 15:01:33 +0300

    m3

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// import
 let posts = require("./data");
 const express = require("express");
 const app = express();
@@ -11,11 +10,9 @@ const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
 const path = require("path");
 
-//configs and init
 dotenv.config();
 const PORT = process.env.PORT;
 
-//middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
@@ -33,15 +30,10 @@ app.use((req, res, next) => {
 connectDb();
 console.log(path.join(__dirname, "media"));
 
-// GET api/posts
-
-//routes
 app.use("/api/posts", postsRoutes);
 
-//middleware
 app.use(notFoundHandler);
 
-// starting the server
 app.listen(PORT, () => {
   console.log(`The application is running on localhost:${PORT}`);
 });
